0ebc7ac0cd864583bacca491b07f3d4a381835ef
CM <cmonterrosad@hotmail.com>
2021-05-28 16:29:54 -0600

    :wrench: ORM connection config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,16 @@
+export const typeOrmConfig  = () => ({
+    database:{
+    type: 'mysql',
+    host: process.env.HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity.ts'],
+    migrationsTableName: 'custom_migration_table',
+    migrations: ["migration/*.js"],
+    cli: {
+        migrationsDir: 'migration'
+    }
+    },
+});
