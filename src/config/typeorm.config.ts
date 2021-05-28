82ae62dc23b0f9490c087ea997f5dc593fa8c135
CM <cmonterrosad@hotmail.com>
2021-05-28 16:56:59 -0600

    :wrench: add support for migrations files

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,13 +2,13 @@ export const typeOrmConfig  = () => ({
     database:{
     type: 'mysql',
     host: process.env.HOST,
-    port: 5432,
+    port: 3307,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity.ts'],
     migrationsTableName: 'custom_migration_table',
-    migrations: ["migration/*.js"],
+    migrations: ['migration/*.js'],
     cli: {
         migrationsDir: 'migration'
     }
