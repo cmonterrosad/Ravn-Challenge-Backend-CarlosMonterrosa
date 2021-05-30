6fdf9280f9a526564aaca10eb4aa69d59ca6232d
CM <cmonterrosad@hotmail.com>
2021-05-30 15:31:18 -0600

    :wrench: migration and typeOrm config file

--- src/config/orm.migration.ts
+++ src/config/orm.migration.ts
@@ -1,36 +1,18 @@
 import { ConnectionOptions } from 'typeorm';
 
-
-// Check typeORM documentation for more information.sudo
 const config: ConnectionOptions = {
     type: 'mysql',
     host: process.env.HOST, // localhost
     port: 3307,
-    username: process.env.DB_USERNAME, // databse login role username
-    password: process.env.DB_PASSWORD, // database login role password
-    database: process.env.DB_DATABASE, // db name
-
-    // entities name should be **.entity.ts
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE,
     entities: [__dirname + '/../entities/*{.js,.ts}'],
-
-    // We are using migrations, synchronize should be set to false.
-    // synchronize: process.env.TYPEORM_SYNCHRONIZE
-    //  ? process.env.TYPEORM_SYNCHRONIZE.toLowerCase() === 'true'
-    //  : false,
     synchronize: false,
-
-    // Run migrations automatically,
-    // you can disable this if you prefer running migration manually.
     migrationsRun: false,
-
-    logging: false,
-    // logger: 'advanced-console',
     migrations: ['src/migrations/*.ts'],
     cli: {
-        // Location of migration should be inside src folder
-        // to be compiled into dist/ folder.
         migrationsDir: 'src/migrations'
     }
 };
-
 export = config;
