1d92cc31ed5340ad674d3de42be7066487855bed
CM <cmonterrosad@hotmail.com>
2021-05-30 17:30:37 -0600

    :wrench: adding db port env file

--- src/config/orm.migration.ts
+++ src/config/orm.migration.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from 'typeorm';
 const config: ConnectionOptions = {
     type: 'mysql',
     host: process.env.HOST, 
-    port: parseInt(process.env.PORT),
+    port: parseInt(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_DATABASE,
--- src/config/typeorm.config.service.ts
+++ src/config/typeorm.config.service.ts
@@ -4,10 +4,11 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    console.log(parseInt(process.env.DB_PORT));
     return {
         type: 'mysql',
         host: process.env.HOST,
-        port: parseInt(process.env.PORT),
+        port: parseInt(process.env.DB_PORT),
         username: process.env.DB_USERNAME, 
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
