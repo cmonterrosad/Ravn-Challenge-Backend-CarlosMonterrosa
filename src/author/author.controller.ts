818eaf339e28cd83ec5e87ceed8737a42c77085f
CM <cmonterrosad@hotmail.com>
2021-05-30 15:58:45 -0600

    :pencil2: add type to controller

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -1,4 +1,5 @@
 import { CacheInterceptor, Controller, DefaultValuePipe, Get, ParseIntPipe, Query, UseInterceptors} from '@nestjs/common';
+import { authors } from 'src/entities/author.entity';
 import { AuthorService } from './author.service';
 
 @Controller('author')
@@ -8,7 +9,7 @@ export class AuthorController {
     constructor(private readonly authorService:AuthorService){}
 
     @Get()
-    getAu(@Query('count',new DefaultValuePipe(10),ParseIntPipe)count:number){
+    getAuthors(@Query('count',new DefaultValuePipe(10),ParseIntPipe)count:number):Promise<authors[]>{
         return this.authorService.getAuthors(count);
     }
 }
